f16e6efdb49e3589840463bcf7785062f981293b
safrio <safrio@gmail.com>
2017-02-28 00:42:47 +0500

    Fixed production mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,10 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "volt_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = true
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
